ce4ba150ec81286f7524f863c188fdbb1fa28f89
Darwie Fang <fangdb@bc.edu>
2020-02-03 11:41:29 -0500

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by darwie fang on 3/2/20.
+//  Copyright © 2020 darwie fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
